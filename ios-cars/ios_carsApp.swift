cff560a0ed3d3582d1668a4f74fdc1ca8060e5b7
efrain mouton <efrain.mouton@student.vives.be>
2025-12-18 11:06:37 +0100

    Initial Commit

---
+++ ios-cars/ios_carsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_carsApp.swift
+//  ios-cars
+//
+//  Created by Efrain Mouton on 18/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ios_carsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
